6834fe97737218e47273ceb73cfcabfe4d5fcbf5
Zhilyasov <zhilyasovit@gmail.com>
2024-01-11 20:16:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './screen/components/App'
+import './screen/styles/index.css'
 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)

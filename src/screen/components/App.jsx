7ccdbf9aae98c8a97373051dde113eb5da34668e
Zhilyasov <zhilyasovit@gmail.com>
2024-01-23 10:06:51 +0300

    localize

--- src/screen/components/App.jsx
+++ src/screen/components/App.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
 import { IntlProvider } from 'react-intl'
+import { LOCALES } from '../../i18n/locales'
+import { messages } from '../../i18n/messages'
 import '../styles/App.css'
 import About from './modules/html/About'
 import Contact from './modules/html/Contact'
@@ -6,37 +9,38 @@ import Footer from './modules/html/Footer'
 import Hero from './modules/html/Hero'
 import Nav from './modules/html/Nav'
 import Projects from './modules/html/Projects'
-import { LOCALES } from '../../i18n/locales'
-import { messages } from '../../i18n/messages'
-import { useState } from 'react'
-
 
 function App() {
 	const [currentLocale, setCurrentLocale] = useState(getInitialLocale())
 
+	const def_lang = navigator.language || navigator.userLanguage
+	let lang = LOCALES.ENGLISH
+	if (def_lang === 'ru') {
+		lang = LOCALES.RUSSIAN
+	} else {
+		lang = LOCALES.ENGLISH
+	}
+
 	const handleChange = ({ target: { value } }) => {
-  setCurrentLocale(value)
-  // сохраняем локацию в хранилище
-  localStorage.setItem('locale', value)
-}
+		setCurrentLocale(value)
+		// сохраняем локацию в хранилище
+		localStorage.setItem('locale', value)
+	}
 
 	function getInitialLocale() {
 		// получаем сохраненные данные
 		const savedLocale = localStorage.getItem('locale')
-		return savedLocale || LOCALES.ENGLISH
+		return savedLocale || lang
 	}
 
-	// let lang = navigator.language || navigator.userLanguage
-	console.log(navigator.language)
-	console.log(navigator.userLanguage)
 	return (
 		<IntlProvider
-      messages={messages[getInitialLocale()]}
-      locale={getInitialLocale()}
-      defaultLocale={LOCALES.ENGLISH}
-    >
+			messages={messages[getInitialLocale()]}
+			locale={getInitialLocale()}
+			defaultLocale={lang}
+		>
 			<div>
-				<Nav currentLocale={currentLocale} handleChange={handleChange}/>
+				<Nav currentLocale={currentLocale} handleChange={handleChange} />
 				<Hero />
 				<About />
 				<Projects />
